675476929f5226092f940d8a631b18adef14bef0
Miguel Angel Suarez Montiel <front_moviles1@aserta.com.mx>
2017-10-23 17:56:51 -0500

    Se agrega primer practica hola mundo en tic

--- public_html/prueba.ts
+++ public_html/prueba.ts
@@ -1,3 +1,9 @@
 function holaMundo(nombre) {
     return "hola " + nombre;
 }
+
+let nombre = "Miguel Suarez";
+var etiqueta = <HTMLElement>document.getElementById("container");
+etiqueta.innerHTML= holaMundo(nombre);
+
+alert('Hola mundo');
